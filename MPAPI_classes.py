8a80ac0c59a642caec2e305240443286749f62b9
Sassberto <matt@vistaseeker.com>
2017-12-31 13:20:33 -0800

    refactoring some common code

---
+++ MPAPI_classes.py
@@ -0,0 +1,61 @@
+import json
+import boto3
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+
+class MPAPI():
+	def __init__(self):
+		return
+		
+	#get a single user profile
+	def get_profile(self, user_id):
+		
+		client = boto3.resource('dynamodb')
+
+		tbl_users = client.Table('User')
+
+		response = tbl_users.get_item(
+			Key={
+				"UserId": int(user_id)
+			}
+		)    
+			
+		item = response.get('Item',0)	
+	
+		return item 
+	
+	#get a single route
+	def get_route(self,route_id):
+		
+		client = boto3.resource('dynamodb')
+		tbl_routes = client.Table('Routes')
+		    
+		response = tbl_routes.get_item(
+			Key={
+				"RouteId": int(route_id)
+			}
+		)    
+				
+		item = response.get('Item',0)	
+		
+		return item
+		
+	#returns ticks with routes	
+	def get_ticks(self,user_id):
+		
+		#show list of ticks for a user
+		client = boto3.resource('dynamodb')
+		tbl_ticks = client.Table('Ticks')
+		
+		response = tbl_ticks.query(
+		   KeyConditionExpression=Key('UserId').eq(user_id)
+		)
+	
+		item = response.get('Items',0)
+				
+		#append the route information
+		for route in item:			
+			routeinfo = MPAPI().get_route(int(route['RouteId']))	
+			route.update(routeinfo)
+			
+		return item
